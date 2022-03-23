c67bc1f6debf0c51910259da320e62150ff09f70
Luke Tellis <luke.tellis@aroflo.com>
2022-03-23 16:40:59 +1100

    Axios install

--- client/src/hooks/useSearchForWeatherByGps.js
+++ client/src/hooks/useSearchForWeatherByGps.js
@@ -1,28 +1,32 @@
 import { MESSAGES } from "../config/constants";
-import { getWeatherByGPS } from "../apis/weatherApis"
+import { getWeatherByGPS } from "../apis/weatherApis";
 
-export async function searchForWeatherByGPS(lat, lon, setWeatherDays, setErrorMessage, searchForDailyWeatherGraphData) {
+export async function searchForWeatherByGPS(
+  lat,
+  lon,
+  setWeatherDays,
+  setErrorMessage,
+  searchForDailyWeatherGraphData
+) {
+  setErrorMessage("");
+  console.log(`Searching for weather with lat: ${lat} lon: ${lon}`);
+
+  try {
+    const response = await getWeatherByGPS(lat, lon);
+
+    if (response.status === 404) {
+      setWeatherDays([]);
+      throw new Error(MESSAGES.WEATHER_NOT_FOUND);
+    }
+    if (response.status !== 200) {
+      throw new Error(MESSAGES.API_ERROR);
+    }
+
+    const weather = response.data;
+    setWeatherDays(weather);
     setErrorMessage("");
-    console.log(`Searching for weather with lat: ${lat} lon: ${lon}`);
-    
-    try {
-      const response = await getWeatherByGPS(lat, lon);
-
-        if (response.status === 404) {
-          setWeatherDays([]);
-          throw new Error(MESSAGES.WEATHER_NOT_FOUND);
-        }
-        if (response.status !== 200) {
-          throw new Error(MESSAGES.API_ERROR);
-        }
-
-        const weather = response.data;
-        setWeatherDays(weather);
-        setErrorMessage("");
-        searchForDailyWeatherGraphData(lat, lon);
-
-    }
-      catch(error) {
-        setErrorMessage(error.message);
-      };
-    }
+    searchForDailyWeatherGraphData(lat, lon);
+  } catch (error) {
+    setErrorMessage(error.message);
+  }
+}
